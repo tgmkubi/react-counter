753f07ad288de3f21c8569498eb594981eba434b
Kubilay Uysal <kubilayuysall@gmail.com>
2023-11-23 16:58:57 +0300

    Outsource. Cntxt & State to Separate Provider Cmp.

--- src/store/counter-context.jsx
+++ src/store/counter-context.jsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
 
 export const CounterContext = createContext({
   count: 0,
@@ -6,3 +7,36 @@ export const CounterContext = createContext({
   decrease: () => {},
   reset: () => {},
 });
+
+export default function CounterContextProvider({ children }) {
+  const [counterState, setCounterState] = useState({
+    count: 0,
+  });
+
+  function increaseCounter() {
+    setCounterState((prevState) => ({ count: prevState.count + 1 }));
+  }
+  function decreaseCounter() {
+    setCounterState((prevState) => ({ count: prevState.count - 1 }));
+  }
+  function resetCounter() {
+    setCounterState({ count: 0 });
+  }
+
+  const ctxValue = {
+    count: counterState.count,
+    increase: increaseCounter,
+    decrease: decreaseCounter,
+    reset: resetCounter,
+  };
+
+  return (
+    <CounterContext.Provider value={ctxValue}>
+      {children}
+    </CounterContext.Provider>
+  );
+}
+
+CounterContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
